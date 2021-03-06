9491c7b348065cb15aa301cdd1f470a486c646e9
Robbie <rob.w.sherman@gmail.com>
2021-03-06 14:00:33 +0000

    Fix header style

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,6 +1,6 @@
-import Link from "next/link";
-import styled from "styled-components";
-import Nav from "./Nav";
+import Link from 'next/link';
+import styled from 'styled-components';
+import Nav from './Nav';
 
 const Logo = styled.h1`
   font-size: 4rem;
@@ -22,7 +22,7 @@ const HeaderStyles = styled.header`
     display: grid;
     grid-template-columns: auto 1fr;
     justify-content: space-between;
-    align-items: center;
+    align-items: stretch;
   }
 
   .sub-bar {
@@ -39,11 +39,11 @@ export default function Header() {
         <Logo>
           <Link href="/">SICK FITS</Link>
         </Logo>
+        <Nav />
       </div>
       <div className="sub-bar">
         <p>search</p>
       </div>
-      <Nav />
     </HeaderStyles>
   );
 }
