3fd6383e726fe4d305e7caa74b03308ca044bb2b
Robbie <rob.w.sherman@gmail.com>
2021-03-17 22:33:43 +0000

    Send a password reset email

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -9,6 +9,7 @@ import { User } from './schemas/User';
 import { Product } from './schemas/Product';
 import { ProductImage } from './schemas/ProductImage';
 import { insertSeedData } from './seed-data';
+import { sendPasswordResetEmail } from './lib/mail';
 
 const databaseURL = process.env.DATABASE_URL;
 
@@ -25,8 +26,8 @@ const { withAuth } = createAuth({
     fields: ['name', 'email', 'password'],
   },
   passwordResetLink: {
-    async sendToken(args) {
-      console.log(args);
+    async sendToken({ token, identity }) {
+      await sendPasswordResetEmail(token, identity);
     },
   },
 });
