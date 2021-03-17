3b68b8620de8151b3cd6ea849911c5ac3b3eb0a9
Robbie <rob.w.sherman@gmail.com>
2021-03-17 19:25:00 +0000

    Fix button text

--- frontend/components/SignUp.js
+++ frontend/components/SignUp.js
@@ -85,7 +85,7 @@ export default function SignUp() {
           />
         </label>
 
-        <button tpye="submit">Sign in!</button>
+        <button tpye="submit">Sign up!</button>
       </fieldset>
     </Form>
   );
