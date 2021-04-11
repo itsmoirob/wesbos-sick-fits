fa074069434d4793b5501e9e91083e373fbb2b43
Robbie <rob.w.sherman@gmail.com>
2021-04-11 14:42:18 +0100

    Refetch user after add to cart

--- frontend/components/AddToCart.js
+++ frontend/components/AddToCart.js
@@ -1,5 +1,6 @@
 import gql from 'graphql-tag';
 import { useMutation } from '@apollo/client';
+import { CURRENT_USER_QUERY } from './User';
 
 const ADD_TO_CART_MUTATION = gql`
   mutation ADD_TO_CART_MUTATION($id: ID!) {
@@ -14,6 +15,7 @@ export default function AddToCart({ id }) {
     variables: {
       id,
     },
+    refetchQueries: [{ query: CURRENT_USER_QUERY }],
   });
 
   return (
