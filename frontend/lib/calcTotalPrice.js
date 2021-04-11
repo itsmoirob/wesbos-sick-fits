06143084c42bc6d8542953ac9a11e695f939433b
Robbie <rob.w.sherman@gmail.com>
2021-04-11 13:33:07 +0100

    FIx total price dispaly

--- frontend/lib/calcTotalPrice.js
+++ frontend/lib/calcTotalPrice.js
@@ -2,6 +2,6 @@ export default function calcTotalPrice(cart) {
   return cart.reduce((total, cartItem) => {
     if (!cartItem.product) return total;
 
-    total += cartItem.product.price * cartItem.quantity;
+    return (total += cartItem.product.price * cartItem.quantity);
   }, 0);
 }
